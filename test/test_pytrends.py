465df4d2f0b6e66e04aa5fe8cde7ffc511e7ced2
route250@mx130 <hanaya.white@gmail.com>
2024-06-21 09:52:49 +0900

    update pytrends

--- test/test_pytrends.py
+++ test/test_pytrends.py
@@ -11,94 +11,46 @@ from pytrends.request import TrendReq
 def decode_and_parse_url(url):
     # Parse the URL
     parsed_url = urlparse(url)
-    
     # Decode the query part
     decoded_query = unquote(parsed_url.query)
-    
     # Parse the decoded query part into a dictionary
     query_params = parse_qs(decoded_query)
-    
     # Convert query parameters to a simple dictionary (not lists of values)
     query_params_dict = {k: v[0] for k, v in query_params.items()}
-    
+
     return query_params_dict
+
+def uniq_words( words ):
+    # 部分文字列を排除
+    result = []
+    for s in words:
+        if not any(s != other and s in other for other in words):
+            result.append(s)
+    return result
 
 # Example usage
 url = "/trends/explore?q=/m/012f86&date=2023-05-01+2023-06-01&geo=JP"
 result = decode_and_parse_url(url)
 print(result)
 
-
-gtrend_list = ['gtrend']
-def chk_gtrend(gtrend):
-    startday = gtrend["startday"]
-    endday = gtrend["endday"]
-    keyword = gtrend["gtrend"]
-
-    pytrends = TrendReq(hl='ja-JP', tz=-540 )
-    kw_list = [keyword]
-    pytrends.build_payload(kw_list, timeframe=f"{startday} {endday}", geo='JP')
-    q = pytrends.related_queries()
-    if q[kw_list[0]]['rising'] is not None:
-        rising_data = q[kw_list[0]]['rising'].values.tolist()
-    else:
-        rising_data = []  # 'rising' のデータが存在しない場合、rising_data は空のリストになります
-    gtrend_info = {
-        "status": "success",
-        "gtrend": gtrend,
-        "rising": rising_data
-    }
-    gtrend_list.append(gtrend)
-    return json.dumps(gtrend_info)
-
-def keyword():
-    pytrends:TrendReq = TrendReq(hl='ja-JP', tz=-540 )
-    w = pytrends.trending_searches(pn='japan')
-    print(w)
-
 def today_searches():
     pytrends:TrendReq = TrendReq(hl='ja-JP', tz=-540 )
     w = pytrends.today_searches(pn='JP')
-    print(w)
+    result=[]
+    for ww in w:
+        params = decode_and_parse_url(ww)
+        q=params.get('q')
+        if q:
+            result.append(q)
+    return uniq_words( result )
 
-def related_keyword( *args ):
-    # キーワードに関連するキーワードを取得する
-    kw:list[str] = []
-    if isinstance(args,(list,tuple)):
-        for w in args:
-            if isinstance(w,str):
-                kw.append(w)
-    if len(kw)==0:
-        return []
-
-    pytrends:TrendReq = TrendReq(hl='ja-JP', tz=-540 )
-    pytrends.build_payload( kw_list=kw, timeframe='2023-05-01 2023-06-01', geo='JP' )
-    queries = pytrends.related_queries()
-
-    topic_dict:list = {}
-    for w in kw:
-        aa:pd.DataFrame = queries[w]['top']
-        for r, row in aa.iterrows():
-            value = row['value']
-            topic = row['topic_title']
-            value = max( value, topic_dict.get(topic,0) )
-            topic_dict[topic] = value
-        bb = queries[w]['rising']
-        for r, row in bb.iterrows():
-            value = row['value']
-            topic = row['topic_title']
-            value = max( value, topic_dict.get(topic,0) )
-            topic_dict[topic] = value
-    # 辞書をintの降順でソートし、キーのリストを作成
-    sorted_keys = [k for k, v in sorted(topic_dict.items(), key=lambda item: item[1], reverse=True)]
-    # 部分文字列を排除
-    result = []
-    for s in sorted_keys:
-        if not any(s != other and s in other for other in sorted_keys):
-            result.add(s)
-    return result
+def related_queries( *args ):
+    return related_keyword_topics( *args, mode=False)
 
 def related_topics( *args ):
+    return related_keyword_topics( *args, mode=True)
+
+def related_keyword_topics( *args, mode:bool=False ):
     # キーワードに関連するキーワードを取得する
     kw:list[str] = []
     if isinstance(args,(list,tuple)):
@@ -107,33 +59,34 @@ def related_topics( *args ):
                 kw.append(w)
     if len(kw)==0:
         return []
-
+    day1='2024-06-01'
+    day2='2024-06-20'
+    tf=f"{day1} {day2}"
     pytrends:TrendReq = TrendReq(hl='ja-JP', tz=-540 )
-    pytrends.build_payload( kw_list=kw, timeframe='2023-05-01 2023-06-01', geo='JP' )
-    queries = pytrends.related_topics()
+    pytrends.build_payload( kw_list=kw, timeframe=tf, geo='JP' )
+    if mode:
+        queries = pytrends.related_topics()
+    else:
+        queries = pytrends.related_queries()
 
     topic_dict:list = {}
     for w in kw:
         aa:pd.DataFrame = queries[w]['top']
         for r, row in aa.iterrows():
             value = row['value']
-            topic = row['topic_title']
+            topic = row.get('query') or row.get('topic_title')
             value = max( value, topic_dict.get(topic,0) )
             topic_dict[topic] = value
         bb = queries[w]['rising']
         for r, row in bb.iterrows():
             value = row['value']
-            topic = row['topic_title']
+            topic = row.get('query') or row.get('topic_title')
             value = max( value, topic_dict.get(topic,0) )
             topic_dict[topic] = value
     # 辞書をintの降順でソートし、キーのリストを作成
     sorted_keys = [k for k, v in sorted(topic_dict.items(), key=lambda item: item[1], reverse=True)]
     # 部分文字列を排除
-    result = []
-    for s in sorted_keys:
-        if not any(s != other and s in other for other in sorted_keys):
-            result.add(s)
-    return result
+    return uniq_words( sorted_keys )
 
 def realtime_trending():
     # category
@@ -146,26 +99,28 @@ def realtime_trending():
     #  t: テクノロジー
     pytrends:TrendReq = TrendReq(hl='ja-JP', tz=-540 )
     w = pytrends.realtime_trending_searches( pn='JP', cat='all', count=5 )
-    #pytrends.related_topics()
-    #pytrends.related_queries()
-    #pytrends.today_searches()
-    #pytrends.top_charts()
-    #pytrends.trending_searches()
-    #pytrends.realtime_trending_searches()
-    print(w)
+
+    result = []
+    for r, row in w.iterrows():
+        names = row['entityNames']
+        for name in names:
+            result.append(name)
+    return uniq_words(result)
 
 def trending_searches():
     pytrends:TrendReq = TrendReq(hl='ja-JP', tz=-540 )
     w = pytrends.trending_searches( pn='japan' )
-    print(w)
+    result = [w for w in w[0]]
+    return uniq_words( result )
 
 def suggestions( keyword ):
     pytrends:TrendReq = TrendReq(hl='ja-JP', tz=-540 )
     #w = pytrends.top_charts( date=2024, hl = 'ja_JP', tz=-540, geo='JP' )
     res:list[dict] = pytrends.suggestions( keyword )
     result_df = pd.DataFrame(res)
-    result_df = result_df.drop(columns=['mid'])
-    print(result_df)
+    title_list = result_df['title']
+    result = [w for w in title_list]
+    return uniq_words(result)
 
 #https://scrapbox.io/PythonOsaka/Python%E3%81%A7GoogleTrends%E3%81%AE%E3%83%87%E3%83%BC%E3%82%BF%E3%82%92%E5%8F%96%E5%BE%97%E3%81%97%E3%81%A6%E3%81%BF%E3%82%88%E3%81%86
 
@@ -177,9 +132,9 @@ def suggestions( keyword ):
 # 		注目 ー は検索ボリュームの上昇率が高いもの
 
 def main():
-    #trending_searches()
-    # suggestions( "ハムスター")
-    related_topics( "健康診断")
+    # trending_searches()
+    suggestions( "ハムスター")
+    # related_topics( "健康診断")
     # realtime_trending()
     #related_keyword( '科学技術' )
     # gtrend = {
@@ -189,6 +144,7 @@ def main():
     # }
     # ret = chk_gtrend(gtrend)
     # print(ret)
+    # today_searches()
 
 if __name__ == "__main__":
     main()
